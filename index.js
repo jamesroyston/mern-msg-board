0283c18071f06eba3a2ede47d411e96fffc25bdd
James Royston <jamesroyston03@gmail.com>
2019-07-24 07:38:20 -0500

    fix

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
-const port = process.env.PORT || 4000
+let port = process.env.PORT || 4000
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
